efe1d4f6322060577e9fc618f25752077cd2e373
Venkata Surya Kiran Kuppili <venkata.kuppili@evry.com>
2019-04-02 12:17:56 +0530

    Project structure is created

---
+++ evry-banking-management-system/src/main/java/com/evry/bank/evrybankingmanagementsystem/EvryBankingManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.evry.bank.evrybankingmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvryBankingManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvryBankingManagementSystemApplication.class, args);
+	}
+
+}
---
+++ evry-banking-management-system/src/test/java/com/evry/bank/evrybankingmanagementsystem/EvryBankingManagementSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.evry.bank.evrybankingmanagementsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EvryBankingManagementSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
