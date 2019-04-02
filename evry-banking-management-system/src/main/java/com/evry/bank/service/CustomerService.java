35453752cdefa0f4dd6e715c8244f7fd8f2f8ad9
Buddamallappa Gari Ucheerappa <buddamallappa.gu@spanservices.com>
2019-04-02 15:06:08 +0530

    service class created

--- evry-banking-management-system/src/main/java/com/evry/bank/service/CustomerService.java
+++ evry-banking-management-system/src/main/java/com/evry/bank/service/CustomerService.java
@@ -1,6 +1,19 @@
 package com.evry.bank.service;
 
+import java.util.List;
+
+import com.evry.bank.model.Customer;
+
 public interface CustomerService {
 
-	
+	public int addCustomer(final Customer customer);
+
+	public Customer getCusomterById(final int id);
+
+	public List<Customer> getAllCustomers();
+
+	public int updateCustomer(final int id, final Customer customer);
+
+	public int deleteCustomer(final int id);
+
 }
---
+++ evry-banking-management-system/src/main/java/com/evry/bank/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,40 @@
+package com.evry.bank.service.impl;
+
+import java.util.List;
+
+import com.evry.bank.model.Customer;
+import com.evry.bank.service.CustomerService;
+
+public class CustomerServiceImpl implements CustomerService {
+
+	@Override
+	public int addCustomer(Customer customer) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Customer getCusomterById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Customer> getAllCustomers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int updateCustomer(int id, Customer customer) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteCustomer(int id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
