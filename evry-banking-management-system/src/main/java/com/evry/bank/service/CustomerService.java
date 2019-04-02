e30bfd7a342cfc169a10b187b721cc6c2d5d087e
Buddamallappa Gari Ucheerappa <buddamallappa.gu@spanservices.com>
2019-04-02 16:12:31 +0530

    updated service class

--- evry-banking-management-system/src/main/java/com/evry/bank/service/CustomerService.java
+++ evry-banking-management-system/src/main/java/com/evry/bank/service/CustomerService.java
@@ -6,13 +6,35 @@ import com.evry.bank.model.Customer;
 
 public interface CustomerService {
 
+	/*
+	 * this method tells store customerdetails
+	 * @return type int
+	 */
 	public int addCustomer(final Customer customer);
 
+	/*
+	 * if we want get perticular customerid so you can using this method
+	 * @return type is Customer
+	 */
 	public Customer getCusomterById(final int id);
+	
+	/*
+	 * here you can get all customer details
+	 * this method return customer object
+	 */
 
 	public List<Customer> getAllCustomers();
+	
+	/*
+	 * here you can update customer details 
+	 * @return type int
+	 */
 
 	public int updateCustomer(final int id, final Customer customer);
+	
+	/*
+	 * delete perticular customer id using this mehtod
+	 */
 
 	public int deleteCustomer(final int id);
 
