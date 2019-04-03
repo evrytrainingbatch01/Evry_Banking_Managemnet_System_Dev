80803b9d4b401b4dbf2b54ac8bd8bad8937e9456
Indrajit Maharana <indrajit.maharana@spanservices.com>
2019-04-03 12:35:43 +0530

    Changes in DAO

--- evry-banking-management-system/src/main/java/com/evry/bank/dao/CustomerDao.java
+++ evry-banking-management-system/src/main/java/com/evry/bank/dao/CustomerDao.java
@@ -1,3 +1,8 @@
+/**
+ * @author indrajit.maharana
+ * @author swaroop.panda
+ * @author parthanjaya.nayak
+ */
 package com.evry.bank.dao;
 
 import java.util.List;
@@ -8,15 +13,19 @@ import com.evry.bank.model.Customer;
 
 public interface CustomerDao extends CrudRepository<Customer,Integer>{
 	
+	// Adding the Customer details as per Customer Entity
 	public int addCustomer(final Customer customer);
 
+	// Retrieving a single customer details by using CUSTOMER_ID
 	public Customer getCusomterById(final int id);
 
+	// Retrieving all the customers with details from the database
 	public List<Customer> getAllCustomers();
 
+	// Updating a single customer details by using CUSTOMER_ID
 	public int updateCustomer(final int id, final Customer customer);
 
+	// Permanently remove a Customer From databasa
 	public int deleteCustomer(final int id);
 	
-
 }
