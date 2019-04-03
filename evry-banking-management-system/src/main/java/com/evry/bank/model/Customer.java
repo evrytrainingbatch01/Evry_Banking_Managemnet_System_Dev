61fadca1d8bc9b539aec5c7b506abb7d28e0aaf4
Indrajit Maharana <indrajit.maharana@spanservices.com>
2019-04-03 11:38:53 +0530

    Added Entity Annotations

--- evry-banking-management-system/src/main/java/com/evry/bank/model/Customer.java
+++ evry-banking-management-system/src/main/java/com/evry/bank/model/Customer.java
@@ -2,20 +2,56 @@ package com.evry.bank.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="eb_customer")
 public class Customer {
 
-	
+	@Id
+	@Column(name = "EB_CUSTOMER_ID")
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@PrimaryKeyJoinColumn
 	private int id;
+	
+	@Column(name = "EB_CUSTOMER_NAME")
 	private String userName;
+	
+	@Column(name = "EB_CUSTOMER_EMAILID")
 	private String email;
+	
+	@Column(name = "EB_CUSTOMER_DOB")
 	private LocalDate dateOfBirth;
+	
+	@Column(name = "EB_CUSTOMER_CITY")
 	private String city;
+	
+	@Column(name = "EB_CUSTOMER_COUNTRY")
 	private String country;
+	
+	@Column(name = "EB_CUSTOMER_MOBILENO")
 	private long mobile;
+	
+	@Column(name = "EB_CUSTOMER_LOGINID")
 	private long customer_id;
+	
+	@Column(name = "EB_CUSTOMER_PASSWORD")
 	private String password;
+	
+	@Column(name = "EB_CUSTOMER_USERTYPE")
 	private int userType;
+	
+	@Column(name = "EB_CUSTOMER_LASTUPDATED")
 	private LocalDate lastUpdate;
+	
+	@Column(name = "EB_CUSTOMER_ACCOUNTSTATUS")
 	private int accountStatus;
 
 	public int getId() {
